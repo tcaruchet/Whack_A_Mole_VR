eb36201441f3e92a3e78f3f3d52e086c07fde1bd
Bastian Ilso <contact@bastianilso.com>
2020-11-10 11:14:22 +0100

    Eye Calibration: Show Grey Background
    To ensure we have maximum contrast, we should display a
    grey background during eye calibration, rather than just the
    bright sky. This creates more contrast.

---
+++ Assets/Plugins/Pupil/Scripts/Utils/EnableDuringCalibration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+    public class EnableDuringCalibration : MonoBehaviour
+    {
+
+        public CalibrationController controller;
+        public MeshRenderer renderer;
+
+        void Awake()
+        {
+            controller.OnCalibrationStarted += EnableMePls;
+            controller.OnCalibrationRoutineDone += DisableMePls;
+        }
+
+        void OnDestroy()
+        {
+            controller.OnCalibrationStarted -= EnableMePls;
+            controller.OnCalibrationRoutineDone -= DisableMePls;
+        }
+
+        void EnableMePls()
+        {
+            renderer.enabled = true;
+        }
+
+        void DisableMePls()
+        {
+            renderer.enabled = false;
+        }
+    }
+}
