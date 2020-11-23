59034e6b71c2c88dba522279b87be603959d944d
Bastian Ilso <hougaard.junior@gmail.com>
2020-11-23 15:23:02 +0100

    Therapist UI: Support Controllers Left/Right/Both
    Revamps the Therapist UI with exposing some raw data
    and most prominently add support for switching between
    left and right controllers (or both).

---
+++ Assets/Scripts/UI/EyePositionUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PupilLabs;
+
+public class EyePositionUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text eyePositionText;
+    private string eyePositionTemplate;
+
+    [SerializeField]
+    private Text eyeInfoText;
+    private string eyeInfoTemplate;
+
+    [SerializeField]
+    private GazeController gazeController;
+
+    [SerializeField]
+    private Transform gazeOrigin;
+
+    private bool isGazing = false;
+    private Vector3 worldGazeOrigin;
+    private Vector3 localGazeDirection;
+    private Vector3 worldGazeDirection;
+    private float gazeConfidence = -1f;
+    public float sphereCastRadius = 0.05f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eyeInfoTemplate = eyeInfoText.text;
+        eyePositionTemplate = eyePositionText.text;
+        gazeController.OnReceive3dGaze += ReceiveGaze;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGazing) {
+            worldGazeOrigin = gazeOrigin.position;
+            worldGazeDirection = gazeOrigin.TransformDirection(localGazeDirection);
+
+            if (Physics.SphereCast(worldGazeOrigin, sphereCastRadius, worldGazeDirection, out RaycastHit hit, Mathf.Infinity))
+            {
+                eyePositionText.text = string.Format(eyePositionTemplate, hit.point.x.ToString("0.00"), hit.point.y.ToString("0.00"), hit.point.z.ToString("0.00"));
+                eyeInfoText.text = string.Format(eyeInfoTemplate, gazeConfidence.ToString("0.00"), hit.transform.gameObject.name);
+            }
+        } else {
+                eyePositionText.text = string.Format(eyePositionTemplate, "-", "-", "-");
+                eyeInfoText.text = string.Format(eyeInfoTemplate, "-", "-");
+        }
+    }
+
+    public void ReceiveGaze(GazeData gazeData) {
+        if (gazeData.MappingContext != GazeData.GazeMappingContext.Binocular)
+        {
+            isGazing = false;
+            return;
+        }
+        isGazing = true;
+        
+        localGazeDirection = gazeData.GazeDirection;
+        gazeConfidence = gazeData.Confidence;
+    }
+}
---
+++ Assets/Scripts/UI/HeadPositionUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadPositionUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text headPosText;
+    private string headPosTemplate;
+
+    [SerializeField]
+    private Text headRotText;
+    private string headRotTemplate;
+
+    [SerializeField]
+    private GameObject headObject;
+
+    private LoggerNotifier loggerNotifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headPosTemplate = headPosText.text;
+        headRotTemplate = headRotText.text;
+        loggerNotifier = new LoggerNotifier();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        headPosText.text = string.Format(headPosTemplate, headObject.transform.position.x.ToString("0.00"), headObject.transform.position.y.ToString("0.00"), headObject.transform.position.z.ToString("0.00"));
+        headRotText.text = string.Format(headRotTemplate, headObject.transform.eulerAngles.x.ToString("0.00"), headObject.transform.eulerAngles.y.ToString("0.00"), headObject.transform.eulerAngles.z.ToString("0.00"));
+    }
+
+    public void LogCurrentPosition() {
+        loggerNotifier.NotifyLogger("Player Position Set", EventLogger.EventType.ModifierEvent);
+    }
+}
