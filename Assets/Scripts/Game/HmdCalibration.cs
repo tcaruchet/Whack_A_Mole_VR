61809ea4c0896c5c42d28b5003a4dce701f14d4e
Bastian Ilso <hougaard.junior@gmail.com>
2022-02-28 15:04:23 +0100

    HmdCalibration: Explicitly calibrate VR player.

---
+++ Assets/Scripts/Game/HmdCalibration.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.Events;
+
+public class HmdCalibration : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class CalibrationEvent : UnityEvent {}
+    [SerializeField]
+    public CalibrationEvent calibrationUpdate;
+
+    [SerializeField]
+    private SteamVR_Input_Sources controller;
+
+    [SerializeField]
+    private GameObject controllerGameObject;
+    bool calibrated = false;
+    int timeout = 100;
+    int time = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controllerGameObject.GetComponent<SteamVR_Behaviour_Pose>().onTransformUpdated.AddListener(delegate{PositionUpdated();});
+    }
+
+    // Update is called once per frame
+    public void PositionUpdated() {
+        // HACK: for some reason we need invoke the calibrationUpdate a couple of times..
+        // need to investigate why this is..
+        if (time < timeout) {
+            if(SteamVR.active)
+            {
+                if (SteamVR_Input._default.inActions.GrabPinch.GetStateDown(controller))
+                {
+                    calibrationUpdate.Invoke();
+                    calibrated = true;
+                }
+            }
+            if (calibrated) {
+            time++;
+            }
+        }
+    }
+}
