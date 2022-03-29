32e1d003e599cd86943dbed01ecb72f5161cde7a
Bastian Ilso <hougaard.junior@gmail.com>
2022-03-29 09:34:15 +0200

    Constraint: Calibrate to controller height
    We were previously calibrating based on
    player height, but to ensure that the
    motorspace aligns, we should Calibrate
    based on controller height instead.

--- Assets/Scripts/Constraint.cs
+++ Assets/Scripts/Constraint.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class Constraint : MonoBehaviour
 {
+
+    // desiredHeight should align with the default height of the motorspace (1.3).
     [SerializeField]
-    private float desiredHeight = 1.6f;
+    private float desiredHeight = 1.3f;
 
     //Assign these variables in the inspector, or find them some other way (eg. in Start() )
     [SerializeField]
     private Transform steamCamera;
+
+    [SerializeField]
+    private Transform steamController;
 
     [SerializeField]    
     private Transform cameraRig;
@@ -28,11 +33,23 @@ public class Constraint : MonoBehaviour
             /*POSITION*/
             // calculate how much to add or subtract from the height, to arrive at y 1.6
             float headHeight = steamCamera.transform.localPosition.y;
-            float heightChange = desiredHeight - headHeight;
+            //float heightChange = desiredHeight - headHeight;
             float xChange = transform.position.x - offsetX;
             float zChange = transform.position.z - offsetZ;
-            transform.position = new Vector3(xChange, heightChange, zChange);
 
+            /*FIT HEIGHT TO MOTORSPACE*/
+            // instead of moving the motorspace, we should move players
+            // such that they have the correct height for using the motorspace.
+
+            // A: determine current player controller height
+            float controllerHeight = steamController.position.y;
+            Debug.Log("SteamController Height: " + steamController.position.y.ToString());
+            // B: determine motorspace (bottom) height
+            float heightChange = desiredHeight - controllerHeight;
+
+            // C: offset player upwards, so the controller aligns with bottom of the motorspace.
+            
+            transform.position = new Vector3(xChange, heightChange, zChange);
             Debug.Log("Seat recentered!");
         }
         else
