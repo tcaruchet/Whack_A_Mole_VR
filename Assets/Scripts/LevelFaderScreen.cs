0be1b86e608a90866673cf11dafebef51c16c06d
lucas <lucas.menot@viacesi.fr>
2022-09-20 11:46:46 +0200

    Creating a fade from / to black effect
    The difference of brightness between the starting Unity logo, and the actual game was too high. To solve this problem we created a fading effect via LevelFaderScreen.cs

---
+++ Assets/Scripts/LevelFaderScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LevelFaderScreen : MonoBehaviour
+{
+    private float _fadeDuration = 2.5f;
+
+    private void Start()
+    {
+        FadeFromBlack();
+    }
+
+    private void FadeToBlack()
+    {
+        //set start color
+        SteamVR_Fade.View(Color.clear, 0f);
+        //set and start fade to
+        SteamVR_Fade.View(Color.black, _fadeDuration);
+    }
+
+    private void FadeFromBlack()
+    {
+        //set start color
+        SteamVR_Fade.View(Color.black, 0f);
+        //set and start fade to
+        SteamVR_Fade.View(Color.clear, _fadeDuration);
+    }
+}
