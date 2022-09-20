f117c861fe39dc5f87635c69599591040f6da020
Xav1204 <73820233+Xav1204@users.noreply.github.com>
2022-09-20 12:59:53 +0200

    Udptating screenshots of the new therapists panel / patient panel
    The different screenshots were not up to date, so we added screenshoting function in Screenshot.cs. To use the screenshot function, we need to press "w" keystroke while playing.

---
+++ Assets/Scripts/Application/ScreenShot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    [SerializeField]
+    private string screenName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Press W to take a Screen Capture
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ScreenCapture.CaptureScreenshot(screenName + ".png");
+            Debug.Log("Screenshot Captured");
+        }
+    }
+}
