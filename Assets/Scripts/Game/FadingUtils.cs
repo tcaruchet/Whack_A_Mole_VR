c10ae1da9f8e470d6ed84180e86951930573b0bb
Bastian Ilso <hougaard.junior@gmail.com>
2023-04-25 14:50:02 +0200

    FadingUtil: Simplify internals
    Simplifies Component use internally
    to make less lookups and pass less
    arguments around among functions.

--- Assets/Scripts/Game/FadingUtils.cs
+++ Assets/Scripts/Game/FadingUtils.cs
@@ -38,53 +38,49 @@ public static class FadingUtils
     }
 
     // Helping functions
-    public static float GetAlpha(ComponentToFade toFade, GameObject Obj) 
+    public static float GetAlpha(Component toFade) 
     {
         float alphaToReturn = 0f;
-        switch (toFade)
-        {
-            case ComponentToFade.CanvasGroup:
-                alphaToReturn=Obj.GetComponent<CanvasGroup>().alpha;
-                break;
-            case ComponentToFade.Image:
-                alphaToReturn=Obj.GetComponent<Image>().color.a;
-                break;
-            case ComponentToFade.SpriteRenderer:
-                alphaToReturn=Obj.GetComponent<SpriteRenderer>().color.a;
-                break;
-            case ComponentToFade.MeshRenderer:
-                alphaToReturn=Obj.GetComponent<MeshRenderer>().material.color.a;
-                break;
-            case ComponentToFade.Text:
-                alphaToReturn=Obj.GetComponent<Text>().color.a;
-                break;
+        if (toFade is CanvasGroup) {
+                CanvasGroup o = (CanvasGroup) toFade;
+                alphaToReturn=o.alpha; 
+        } else if (toFade is Image) {
+                Image o = (Image) toFade;
+                alphaToReturn=o.color.a;
+        } else if (toFade is SpriteRenderer) {
+                SpriteRenderer o = (SpriteRenderer) toFade;
+                alphaToReturn=o.color.a;
+        } else if (toFade is MeshRenderer) {
+                MeshRenderer o = (MeshRenderer) toFade;
+                alphaToReturn=o.material.color.a;
+        } else if (toFade is Text) {
+                Text o = (Text) toFade;
+                alphaToReturn=o.color.a;
         }
         return alphaToReturn;
     } // reads alpha value of an object
 
-    public static void SetAlpha(ComponentToFade toFade, GameObject Obj, float targetAlpha) 
+    public static void SetAlpha(Component toFade, float targetAlpha) 
     {
-        switch (toFade)
-        {
-            case ComponentToFade.CanvasGroup:
-                Obj.GetComponent<CanvasGroup>().alpha=targetAlpha;
-                break;
-            case ComponentToFade.Image:
-                var compColor = Obj.GetComponent<Image>().color;
-                Obj.GetComponent<Image>().color=new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
-                break;
-            case ComponentToFade.SpriteRenderer:
-                compColor = Obj.GetComponent<SpriteRenderer>().color;
-                Obj.GetComponent<SpriteRenderer>().color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
-                break;
-            case ComponentToFade.MeshRenderer:
-                compColor = Obj.GetComponent<MeshRenderer>().material.color;
-                Obj.GetComponent<MeshRenderer>().material.color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
-                break;
-            case ComponentToFade.Text:
-                compColor = Obj.GetComponent<Text>().color;
-                Obj.GetComponent<Text>().color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
-                break;
+        if (toFade is CanvasGroup) {
+                CanvasGroup o = (CanvasGroup) toFade;
+                o.alpha=targetAlpha;
+        } else if (toFade is Image) {
+                Image o = (Image) toFade;
+                var compColor = o.color;
+                o.color=new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
+        } else if (toFade  is SpriteRenderer) {
+                SpriteRenderer o = (SpriteRenderer) toFade;
+                var compColor = o.color;
+                o.color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
+        } else if (toFade is MeshRenderer) {
+                MeshRenderer o = (MeshRenderer) toFade;
+                var compColor = o.material.color;
+                o.material.color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
+        } else if (toFade is Text) {
+                Text o = (Text) toFade;
+                var compColor = o.color;
+                o.color = new Color(compColor.r, compColor.g, compColor.b, targetAlpha);
         }
     } // sets alpha value of an object
 
@@ -93,20 +89,21 @@ public static class FadingUtils
         SetEnable setEnable = SetEnable.Off) 
     { 
         instance=handler;
+
         switch (fadeDirection)
         {
             case FadeAction.In:
-                instance.StartCoroutine(FadeInColor(Obj, FadeIdentification(Obj), fadeTime, fadeDelay, setEnable));
+                instance.StartCoroutine(FadeInColor(Obj, fadeTime, fadeDelay, setEnable));
                 break;
 
             case FadeAction.Out:
-                instance.StartCoroutine(FadeOutColor(Obj, FadeIdentification(Obj), fadeTime, fadeDelay, setEnable));
+                instance.StartCoroutine(FadeOutColor(Obj, fadeTime, fadeDelay, setEnable));
                 break;
         }
     }
 
     // Identification function of the object to fade
-    public static ComponentToFade FadeIdentification(GameObject Obj) 
+    public static Component ComponentIdentification(GameObject Obj) 
     {
         Component[] listComponents = Obj.GetComponents(typeof(Component)); // create a list of all components located inside the object to fade
 
@@ -115,77 +112,75 @@ public static class FadingUtils
         while (iterator<listComponents.Length) // browse all components of the gameObject
         {
             if (listComponents[iterator] is CanvasGroup)
-                return ComponentToFade.CanvasGroup;
+                return listComponents[iterator];
             if (listComponents[iterator] is Image)
-                return ComponentToFade.Image;
+                return listComponents[iterator];
             if (listComponents[iterator] is SpriteRenderer)
-                return ComponentToFade.SpriteRenderer;
+                return listComponents[iterator];
             if (listComponents[iterator] is MeshRenderer)
-                return ComponentToFade.MeshRenderer;
+                return listComponents[iterator];
             if (listComponents[iterator] is Text)
-                return ComponentToFade.Text;
+                return listComponents[iterator];
             iterator++;
         }
-        return ComponentToFade.ERROR; // if no compatible component is found
+        return null; // if no compatible component is found
     }
 
     // Generic fading in function
-    public static IEnumerator FadeInColor(GameObject Obj, ComponentToFade componentToUse, float fadeTime, float fadeDelay, SetEnable setEnable) 
+    public static IEnumerator FadeInColor(GameObject Obj, float fadeTime, float fadeDelay, SetEnable setEnable) 
     {
         if (fadeDelay!=0) // play delay
         {
             yield return new WaitForSeconds(fadeDelay);
         }
+
+        Component componentToUse = ComponentIdentification(Obj);
+        float baseAlpha = GetAlpha(componentToUse); // getting "base" alpha for of the object
+        float alpha;
 
         if (setEnable == SetEnable.On) // enabling
         {
             Obj.SetActive(true);
+            SetAlpha(componentToUse, 0); // setting up alpha to 0 so we can fade in
         }
 
-        SetAlpha(componentToUse, Obj, 0); // setting up alpha to 0 so we can fade in
-
-        for (float i = 0;i<=fadeTime;i+=Time.deltaTime) // play fade effect
-        {
-            SetAlpha(componentToUse, Obj, FadeInAlpha(i, fadeTime, GetAlpha(componentToUse, Obj)));
+        for (float i = 0;i<fadeTime;i+=Time.deltaTime) // play fade effect
+        {  
+            alpha = math.remap(0, fadeTime, baseAlpha, 1f, i);
+            SetAlpha(componentToUse, alpha);
             yield return null;
         }
+
+        SetAlpha(componentToUse, 1f);
     }
 
     // Generic fading out function
-    public static IEnumerator FadeOutColor(GameObject Obj, ComponentToFade componentToUse, float fadeTime, float fadeDelay, SetEnable setEnable) 
+    public static IEnumerator FadeOutColor(GameObject Obj, float fadeTime, float fadeDelay, SetEnable setEnable) 
     {
-        float baseAlpha = GetAlpha(componentToUse, Obj); // getting "base" alpha for of the object
-
         if (fadeDelay!=0) // play delay
         {
             yield return new WaitForSeconds(fadeDelay);
         }
 
-        SetAlpha(componentToUse, Obj, baseAlpha); // Setting alpha to base alpha so we can fade out
+        Component componentToUse = ComponentIdentification(Obj);
+        float baseAlpha = GetAlpha(componentToUse); // getting "base" alpha for of the object
+        float alpha;
 
-        for (float i = 0;i<=fadeTime;i+=Time.deltaTime) // play fade effect
+        //SetAlpha(componentToUse, Obj, baseAlpha); // Setting alpha to base alpha so we can fade out
+
+        for (float i = 0;i<fadeTime;i+=Time.deltaTime) // play fade effect
         {
-            SetAlpha(componentToUse, Obj, FadeOutAlpha(i, fadeTime, GetAlpha(componentToUse, Obj)));
+            alpha = 1-math.remap(0, fadeTime, 0f, baseAlpha, i);
+            Debug.Log("fadeTime: " + fadeTime + "alpha " + alpha + "i " + i);
+            SetAlpha(componentToUse, alpha); //FadeOutAlpha(i, fadeTime, GetAlpha(componentToUse, Obj)));
             yield return null;
         }
+
+        SetAlpha(componentToUse, 0f);
 
         if (setEnable == SetEnable.On) // disabling
         {
             Obj.SetActive(false);
         }
-    }
-
-    // Does the actual alpha shifting towards 1 (apparent object)
-    public static float FadeInAlpha(float iterator, float fadeTime, float alpha) 
-    {
-        alpha=math.remap(0, fadeTime, 0, 1, iterator);
-        return alpha;
-    }
-
-    // Does the actual alpha shifting towards 0 (transparent object)
-    public static float FadeOutAlpha(float iterator, float fadeTime, float alpha) 
-    {
-        alpha=1-math.remap(0, fadeTime, 0, 1, iterator);
-        return alpha;
     }
 }
