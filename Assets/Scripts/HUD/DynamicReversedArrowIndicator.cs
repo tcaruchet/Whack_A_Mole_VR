44f511a4511cb68cd32f501c72a22676b3e78d5e
mrvilix06 <smaurin18@hotmail.fr>
2023-07-28 10:37:44 +0200

    Initial Commit of Out Of Bounds
    Introduces DynamicCenterPointingIndicator,
    DynamicReversedArrowIndicator and
    StaticArrowsIndicator

---
+++ Assets/Scripts/HUD/DynamicReversedArrowIndicator.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.HUD
+{
+    internal class DynamicReversedArrowIndicator : OutOfBoundIndicator
+    {
+        [SerializeField]
+        private WallManager wallManager;
+
+        [SerializeField]
+        private BasicPointer controller;
+
+        [SerializeField]
+        private CanvasGroup arrow;
+
+        [SerializeField]
+        public Color Color; // The color of the arrow //doing nothing with this yet
+
+
+        private Coroutine coroutine;
+        private bool active = false;
+        private bool lastEnter = true;
+        private Side lastSide;
+        private WallInfo wallInfo;
+        private float fadeTime = 0.35f;
+
+        private void Awake()
+        {
+            //arrow = GetComponent<CanvasGroup>();
+            arrow.gameObject.SetActive(false); // Ensure the arrow is disabled by default
+            wallInfo = wallManager.CreateWallInfo();
+        }
+
+        private void OnEnable()
+        {
+            wallManager.stateUpdateEvent.AddListener(OnWallUpdated);
+            OnWallUpdated(wallManager.CreateWallInfo());
+        }
+
+        
+        private void OnDisable()
+        {
+            wallManager.stateUpdateEvent.RemoveListener(OnWallUpdated);
+        }
+
+        private void Update()
+        {
+            if (arrow.gameObject.activeInHierarchy) // Only update rotation if the arrow is active
+            {
+                // Update arrow position to the center of the wall
+                arrow.transform.position = new Vector3(wallInfo.meshCenter.x, wallInfo.meshCenter.y, arrow.transform.position.z);
+                Vector3 controllerPosition = controller.MappedPosition; // The position of the pointer when the method is called
+                Vector3 targetPosition = new Vector3(controllerPosition.x, controllerPosition.y, arrow.transform.position.z);
+
+                // Calculate direction from arrow to target position
+                Vector3 direction = (targetPosition - arrow.transform.position).normalized;
+
+                // Calculate angle in degrees
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                // Apply the rotation to the child GameObject
+                arrow.transform.Find("RotationOffset").transform.eulerAngles = new Vector3(0, 0, angle - 90);
+
+            }
+        }
+
+
+
+
+        /// <summary>Displays an arrow that points towards a target position. The arrow is positioned so that it points towards the target, and it fades in over a specified amount of time.</summary>
+        /// <param name="position">The position of the target.</param>
+        /// <param name="motorSpaceCenter">The center of the motor space.</param>
+        /// <param name="side">The side of the wall that the target is on.</param>
+        /// <remarks>This method is called by the <see cref="BubbleDisplay"/> when the user exiting the NotorSpace.</remarks>
+        internal override void ShowIndicator(Vector3 position, Vector3 motorSpaceCenter, Side side)
+        {
+            if (!arrow.gameObject.activeInHierarchy) // Only show the indicator if it's not already shown
+            {
+                arrow.gameObject.SetActive(true); // Enable the arrow
+
+                arrow.transform.rotation = Quaternion.identity; // Reset the rotation
+                
+                Debug.Log("SHOW INDICATOR Wall center position: " + wallInfo.meshCenter);
+                
+                //reset position
+                arrow.transform.position = new Vector3(wallInfo.meshCenter.x, wallInfo.meshCenter.y, arrow.transform.position.z);
+                Vector3 pointerPosition = position; // The position of the pointer when the method is called
+                arrow.transform.right = pointerPosition - arrow.transform.position;
+
+
+                if (coroutine != null)
+                {
+                    StopCoroutine(coroutine);
+                }
+                coroutine = FadingUtils.FadeRoutine(handler: this, Obj: arrow.gameObject, fadeTime: fadeTime, fadeDirection: FadeAction.In);
+            }
+        }
+
+
+        internal override void HideIndicator()
+        {
+            if (arrow.gameObject.activeInHierarchy) // Only hide the indicator if it's currently shown
+            {
+                if (coroutine != null)
+                {
+                    StopCoroutine(coroutine);
+                }
+                coroutine = FadingUtils.FadeRoutine(handler: this, Obj: arrow.gameObject, fadeTime: fadeTime, fadeDirection: FadeAction.Out);
+                arrow.gameObject.SetActive(false); // Disable the arrow
+            }
+        }
+
+        private void OnWallUpdated(WallInfo w)
+        {
+            wallInfo = w;
+            active = wallInfo.active;
+            if (!lastEnter && active)
+            {
+                ShowIndicator(Vector3.zero, w.wallCenter, lastSide);
+            }
+            else
+            {
+                HideIndicator();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/StaticArrowsIndicator.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.HUD
+{
+    internal class StaticArrowsIndicator : OutOfBoundIndicator
+    {
+        // Reference to the arrow prefab
+        [SerializeField]
+        private GameObject OutOfBoundPrefab;
+
+        // Reference to the parent container for the arrows
+        [SerializeField]
+        private GameObject OutOfBoundContainer;
+
+        // The number of arrows to spawn
+        [SerializeField]
+        private int numberOfObjects;
+
+        // Distance from the center to position the arrows
+        [SerializeField]
+        private float arrowDistance = 1.5f;
+
+        [SerializeField]
+        private WallManager wallManager;
+        private WallInfo wallInfo;
+
+        // Store the last MotorSpace center position and the exited side
+        private Side lastSide;
+
+        [SerializeField]
+        public Color Color;
+
+        [SerializeField]
+        private float fadeTime = 0.35f;
+
+        [SerializeField]
+        private LaserMapper laserMapper;
+
+        private Coroutine fadeCoroutine;
+        private Coroutine fadeRoutine = null;
+        private Coroutine animationRoutine = null;
+
+
+        void OnEnable()
+        {
+            wallManager.stateUpdateEvent.AddListener(OnWallUpdated);
+            OnWallUpdated(wallManager.CreateWallInfo());
+        }
+
+        void OnDisable()
+        {
+            wallManager.stateUpdateEvent.RemoveListener(OnWallUpdated);
+        }
+
+        private void OnWallUpdated(WallInfo w)
+        {
+            wallInfo = w;
+        }
+
+        private void Awake()
+        {
+            // Spawn the arrows and parent them to the container
+            for (int i = 0; i < numberOfObjects; i++)
+            {
+                OutOfBoundPrefab.GetComponent<SpriteRenderer>().color = Color;
+                var obj = Instantiate(OutOfBoundPrefab, OutOfBoundContainer.transform);
+                obj.name = $"Arrow{i}";
+            }
+        }
+
+        /// <summary>
+        /// Stops any previous fade or animation routine and starts new fade in and animation routine.
+        /// The OutOfBoundContainer object is faded in over a specified amount of time, and the arrows are animated in a circular pattern around the OutOfBoundContainer.
+        /// The motor space is also shown in the scene.
+        /// </summary>
+        /// <param name="position">The position of the target.</param>
+        /// <param name="motorSpaceCenter">The center of the motor space.</param>
+        /// <param name="side">The side of the wall that the target is on.</param>
+        /// <remarks>This method is called by the <see cref="BubbleDisplay"/> when the user exiting the NotorSpace.</remarks>
+        internal override void ShowIndicator(Vector3 position, Vector3 motorSpaceCenter, Side side)
+        {
+            // Stop any previous fade or animation routine
+            if (fadeRoutine != null)
+                StopCoroutine(fadeRoutine);
+            if (animationRoutine != null)
+                StopCoroutine(animationRoutine);
+
+            // Start new fade in and animation routine
+            fadeRoutine = StartCoroutine(FadeInObject(OutOfBoundContainer, 10f));
+            animationRoutine = StartCoroutine(OutOfBoundAnimation(true));
+            laserMapper.ShowMotorspace(true);
+        }
+
+
+        internal override void HideIndicator()
+        {
+            // Stop any previous fade or animation routine
+            if (fadeRoutine != null)
+                StopCoroutine(fadeRoutine);
+            if (animationRoutine != null)
+                StopCoroutine(animationRoutine);
+
+            // Start new fade out routine
+            fadeRoutine = StartCoroutine(FadeOutObject(OutOfBoundContainer, 10f));
+            laserMapper.ShowMotorspace(false);
+        }
+
+
+    /// <summary>
+    /// This method is used to animate the arrows that indicate when a user's input is out of bounds.
+    /// The method uses a while loop to continuously instantiate arrows in a circular pattern around the OutOfBoundContainer object.
+    /// The InstantiateInCircle method is called to create the arrows and position them around the OutOfBoundContainer.
+    /// The method takes three parameters: the OutOfBoundContainer object, the number of children in the container, and the width of the motor space plus 0.1f.
+    /// The foreach loop is then used to iterate through each child in the OutOfBoundContainer and perform the animation.
+    /// The current position of the child is stored in a variable called currentPos, and a new Vector3 object is created using the x, y, and z values of the current position.
+    /// The child's position is then set to a new position that is calculated using Vector3.Lerp, which interpolates between the current position and the motor space center position.
+    /// The difference between the motor space center and the child's position is then calculated, and the rotationZ variable is set to the angle between the two points in degrees.
+    /// Finally, the child's rotation is set to a new Quaternion object that is created using the Euler angles (0.0f, 0.0f, rotationZ).
+    /// </summary>
+    /// <param name="isOutOfBound">A boolean value that indicates whether the user's input is out of bounds.</param>
+    /// <returns>An IEnumerator object that can be used to animate the arrows.</returns>
+        private IEnumerator OutOfBoundAnimation(bool isOutOfBound)
+        {
+            while (isOutOfBound)
+            {
+                InstantiateInCircle(OutOfBoundContainer, OutOfBoundContainer.transform.childCount, laserMapper.GetMotorSpace().width + 0.1f);
+
+                foreach (Transform child in OutOfBoundContainer.transform)
+                {
+                    var motorSpaceCenter = laserMapper.transform.position;
+
+                    var currentPos = child.position;
+                    var pointA = new Vector3(currentPos.x, currentPos.y, currentPos.z);
+                    child.position = Vector3.Lerp(pointA, motorSpaceCenter, Mathf.PingPong(Time.time * 0.3f, 0.15f));
+
+                    var difference = motorSpaceCenter - child.position;
+                    float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+                    child.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+                }
+
+                yield return null;
+            }
+        }
+
+        public void InstantiateInCircle(GameObject container, Vector3 location, int howMany, float radius, float zPosition)
+        {
+            int i = 0;
+            float angleSection = (Mathf.PI * 2f / howMany);
+            foreach (Transform prefab in container.transform)
+            {
+                float angle = i * angleSection + (3 * Mathf.PI / 4);
+                Vector3 newPos = location + new Vector3(Mathf.Cos(angle) * 1.5f, Mathf.Sin(angle), 0) * radius;
+                newPos.z = zPosition;
+                prefab.transform.position = newPos;
+                i++;
+            }
+        }
+
+        public void InstantiateInCircle(GameObject container, Vector3 location, int howMany, float radius)
+        {
+            InstantiateInCircle(container, location, howMany, radius, location.z);
+        }
+
+        public void InstantiateInCircle(GameObject container, int howMany, float radius)
+        {
+            InstantiateInCircle(container, laserMapper.transform.position, howMany, radius);
+        }
+
+
+
+        public IEnumerator FadeOutObject(GameObject gameObjects, float fadeSpeed)
+        {
+            foreach (Transform o in gameObjects.transform)
+            {
+                while (o.GetComponent<Renderer>().material.color.a > 0)
+                {
+                    Color objectColor = o.GetComponent<Renderer>().material.color;
+                    float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                    objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                    o.GetComponent<Renderer>().material.color = objectColor;
+                    yield return null;
+                }
+            }
+
+            gameObjects.SetActive(false);
+        }
+
+        public IEnumerator FadeInObject(GameObject gameObjects, float fadeSpeed)
+        {
+            gameObjects.SetActive(true);
+
+            foreach (Transform o in gameObjects.transform)
+            {
+                while (o.GetComponent<Renderer>().material.color.a < 1)
+                {
+                    Color objectColor = o.GetComponent<Renderer>().material.color;
+                    float fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                    objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                    o.GetComponent<Renderer>().material.color = objectColor;
+                    yield return null;
+                }
+            }
+        }
+
+    }
+}
